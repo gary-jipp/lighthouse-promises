17fac06c491053b51b6a1b7dfa55fb64f12bc090
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-15 00:14:18 -0700

    Update

--- promises.js
+++ promises.js
@@ -11,7 +11,7 @@ const url1 = "https://api.kanye.rest/";
 const url2 = "https://www.affirmations.dev/";
 const url3 = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
-const promise1 = axios.get(url1).catch(e => e);
+const promise1 = axios.get(url1).catch(e => e); // what's this??
 const promise2 = axios.get(url2).catch(e => e);
 const promise3 = axios.get(url3).catch(e => e);
 
@@ -20,7 +20,6 @@ Promise.all([promise1, promise2, promise3])
     console.log("Kanye:", error(res[0]) || res[0].data.quote);
     console.log("Affirm:", error(res[1]) || res[1].data.affirmation);
     console.log("Ron:", error(res[2]) || res[2].data[0]);
-  })
-  .catch(e => console.log(e.errno));
+  }); // no catch needed ?
 
 console.log("\n***End of User Thread ***\n");
