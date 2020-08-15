ea172356c4321b58619042a04f6ffbea04176320
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-15 12:32:06 -0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lighthouse-promises
+
+## Catching Promise.all()
+
+- sometimes you want to *not* stop if one of the promises rejects
+
+- knowing how 'return from a catch' works, we can do this
+
+- we pre-catch each promise.  Now that promise can *only* fire a `then` or `finally`
+
+- you may not always need this, but good to know you can
--- promises.js
+++ promises.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+console.log("\n***Start of my Code ***");
 
 const error = function (res) {
   if (!res.data) {
@@ -6,7 +7,6 @@ const error = function (res) {
   }
 };
 
-console.log("\n***Start of User Thread ***\n");
 const url1 = "https://api.kanye.rest/";
 const url2 = "https://www.affirmations.dev/";
 const url3 = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
@@ -14,12 +14,18 @@ const url3 = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
 const promise1 = axios.get(url1).catch(e => e); // what's this??
 const promise2 = axios.get(url2).catch(e => e);
 const promise3 = axios.get(url3).catch(e => e);
+const results={};
 
 Promise.all([promise1, promise2, promise3])
   .then(res => {
-    console.log("Kanye:", error(res[0]) || res[0].data.quote);
-    console.log("Affirm:", error(res[1]) || res[1].data.affirmation);
-    console.log("Ron:", error(res[2]) || res[2].data[0]);
-  }); // no catch needed ?
+    results.r1 = error(res[0]) || res[0].data.quote;
+    results.r2 = error(res[1]) || res[1].data.affirmation;
+    results.r3 = error(res[2]) || res[2].data[0];
 
-console.log("\n***End of User Thread ***\n");
+    console.log("Kanye:", results.r1);
+    console.log("Affirm:", results.r2);
+    console.log("Ron:", results.r3);
+  })
+  .catch(e => console.log("catch:", e)); // maybe some other error?
+
+console.log("***End of my Code ***\n");
