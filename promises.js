19e6bf922db2e796d89d9515539a203b5991f088
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-14 20:30:53 -0700

    Update

--- promises.js
+++ promises.js
@@ -6,24 +6,24 @@ const myPromise = function (value) {
   if (value < 0) {
     return Promise.reject("Bad Value!: " + value);
   }
-  return Promise.resolve("Promise Complete: " + value);
+  return Promise.resolve(value);
 };
 
 // Handle the promise chain
 myPromise(1).
   then(res => {
-    console.log(res);
+    console.log("then: ", res);
     return myPromise(2);
   })
   .then(res => {
-    console.log(res);
+    console.log("then: ", res);
     return myPromise(-3);
   })
   .then(res => {
-    console.log(res);
+    console.log("then: ", res);;
   })
   .catch(err => {
-    console.log(err);
+    console.log("catch", err);
   })
   .finally(() => {
     console.log("finally");
