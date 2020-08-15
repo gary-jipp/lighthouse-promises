7781adaa2bd10ff835f90b877810f9973b44f64d
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-14 20:39:31 -0700

    Update

--- promises.js
+++ promises.js
@@ -1,13 +1,5 @@
-
+const { myPromise } = require('./myPromise');
 console.log("\n***Start of User Thread ***");
-
-// function that returns a promise. Rejects bad values
-const myPromise = function (value) {
-  if (value < 0) {
-    return Promise.reject("Bad Value!: " + value);
-  }
-  return Promise.resolve(value);
-};
 
 // Handle the promise chain
 myPromise(1).
@@ -17,7 +9,7 @@ myPromise(1).
   })
   .then(res => {
     console.log("then: ", res);
-    return myPromise(-3);
+    return myPromise(3);
   })
   .then(res => {
     console.log("then: ", res);;
