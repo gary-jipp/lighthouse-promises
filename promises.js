6b270e17a19624f82baa69d72bf97ce5f42fbccc
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-18 12:34:04 -0700

    Update

--- promises.js
+++ promises.js
@@ -3,7 +3,16 @@ console.log("\n***Start of my Code ***");
 // Create a resolved promise - resolves to a String
 const promise = Promise.resolve("Promise Complete");
 
-// The most basic of promise handling
+// Callback Function to pass to then()
+const callback = function (res) {
+  console.log(res);
+};
+
+// Lets see what my promise has to say
+promise.then(callback);
+
+// of course, we seldom use named callback functions
+// We use anonymous arrow functions
 promise.then(res => {
   console.log(res);
 });
