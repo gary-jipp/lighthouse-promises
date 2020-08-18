7b082839e0f5fa1c61a9204c5c18ac358ac90f67
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-18 16:39:08 -0700

    Update

--- promises.js
+++ promises.js
@@ -1,12 +1,6 @@
 const axios = require("axios");
 console.log("\n***Start of my Code ***");
 
-const error = function (res) {
-  if (!res.data) {
-    return res.response.status + ": " + res.response.statusText;
-  }
-};
-
 const url1 = "https://api.kanye.rest/";
 const url2 = "https://www.affirmations.dev/";
 const url3 = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
@@ -18,13 +12,10 @@ const results = {};
 
 Promise.all([promise1, promise2, promise3])
   .then(res => {
-    results.r1 = error(res[0]) || res[0].data.quote;
-    results.r2 = error(res[1]) || res[1].data.affirmation;
-    results.r3 = error(res[2]) || res[2].data[0];
-
-    console.log("Kanye:", results.r1);
-    console.log("Affirm:", results.r2);
-    console.log("Ron:", results.r3);
+    // Now we have to handle both situations (fulfill or reject)
+    console.log("Kanye:", res[0].data ? res[0].data.quote : res[0]);
+    console.log("Affirm:", res[1].data ? res[1].data.affieration : res[1]);
+    console.log("Ron:", res[2].data ? res[2].data[0] : res[2]);
   })
   // No promise will fire this catch.  But maybe something in the then() will
   .catch(e => console.log("catch:", e));
